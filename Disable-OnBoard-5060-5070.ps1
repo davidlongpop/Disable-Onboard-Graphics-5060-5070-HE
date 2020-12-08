dc565e11579713713339ff344bc5028c53098a55
David Long <49162055+davidlongpop@users.noreply.github.com>
2020-12-08 14:53:40 -0800

    Update Disable-OnBoard-5060-5070.ps1

--- Disable-OnBoard-5060-5070.ps1
+++ Disable-OnBoard-5060-5070.ps1
@@ -1,3 +1,11 @@
+<#
+.Synopsis
+   Disables OnBoard graphics in 5060/5070 Desktop systems when AMD RX500 Series Add-In Card is present
+.DESCRIPTION
+   This script was made as a fix to LoadLibrary issues with AMD Graphics cards when accessed via remote desktop with Intel OnBoard Graphics also present
+.AUTHOR
+   David Long 12-08-2020
+#>
 $addInGraphics = Get-PnpDevice -class Display | Where-Object{$_.Name -like "*RX5*"}
 $onBoardGraphics = Get-PnpDevice -class Display | Where-Object{$_.Name -like "Intel*UHD*"}
 
